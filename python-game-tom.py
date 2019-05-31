572cfee8bf4e3348025285e1362de2464313b0ac
tom-mcmillon <51227445+tom-mcmillon@users.noreply.github.com>
2019-05-31 07:26:12 -0700

    Add files via upload

---
+++ python-game-tom.py
@@ -0,0 +1,17 @@
+print('Hello Python World')
+print('Welcome to the number guessing game')
+print('Please enter a number from 0 to 1000')
+myguess = input()
+myintguess = int(myguess)
+answer = 146
+while myguess != answer:
+    print('Incorrect Answer')
+    print('Please Try Again')
+    newguess = input()
+    newguessint = int(newguess)
+    if newguessint == answer:
+        break
+
+print('Congragulations on Answering Correctly')
+
+print ('You Win a Bad of Dicks')
